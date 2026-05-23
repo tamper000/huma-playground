d174abf54668e503a79a98fd1295cebdc28f156b
tamper000 <sshzxcroot@proton.me>
2026-05-23 17:28:33 +0700

    feat: graceful shutdown

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"test/internal/handler"
 	"test/internal/repository/cache"
+	"time"
 
 	"github.com/bytedance/sonic"
 	"github.com/danielgtaylor/huma/v2"
@@ -77,5 +82,31 @@ func main() {
 	}, handlers.RedirectLink)
 
 	// Start server
-	http.ListenAndServe("127.0.0.1:8888", router)
+	server := &http.Server{
+		Addr:    ":8888",
+		Handler: router,
+
+		ReadTimeout:       time.Second * 5,
+		ReadHeaderTimeout: time.Second * 5,
+		WriteTimeout:      time.Second * 5,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutdown...")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatal(err)
+	}
 }
